6134b514194fa8bb149500ab4e58226206f3aa26
nyxenn <jordib.edu@gmail.com>
2021-01-11 17:37:24 +0100

    Added example.constants.ts

---
+++ src/constants/example.constants.ts
@@ -0,0 +1,8 @@
+// src/constants/pokeApi.constants.ts
+export const PORT = 9999;
+export const WELCOME_MESSAGE = "Welcome to PokeAPI REST";
+export const DB = "pokemon";
+export const DB_USER = "<USERNAME>";
+export const DB_USER_PASS = "<PASSWORD>";
+export const DB_HOST = "localhost";
+export const DB_DIALECT = "postgres";
